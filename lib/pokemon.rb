e134b8ec7821541d11d45de51d8cece3be4cb1d0
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-26 16:47:29 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
+require 'pry'
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db, :hp
+  
+  def initialize(x)
+    
+    @id = x[:id] 
+    @name = x[:name] 
+    @type = x[:type] 
+    @db = x[:db]
+    
+    if x[:hp] then @hp = x[:hp] else nil end 
+      
+  end
+  
+  def self.save(name, type, db)
+  
+    db.execute("INSERT INTO pokemon (name, type)
+      VALUES (?, ?)", name, type)
+  
+  end  
+  
+  def self.find(id, db)
+    result = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    db.execute("ALTER TABLE pokemon ADD hp integer NOT NULL DEFAULT(60)")
+    store = {:id => result[0][0], :name => result[0][1], :type => result[0][2], :db => db, :hp => 60}
+    Pokemon.new(store)
+  end 
 end
