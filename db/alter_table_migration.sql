729f3ee8921cfce322642dea0ef630bd0fb49a0b
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-26 17:07:39 +0000

    Done.

--- db/alter_table_migration.sql
+++ db/alter_table_migration.sql
@@ -1,4 +1,4 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
 
-CREATE TABLE IF NOT EXISTS pokemon (id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+ALTER TABLE pokemon ADD hp integer NOT NULL DEFAULT(60);
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,37 @@
+require 'pry'
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db, :hp
+  
+  def initialize(x)
+    
+    @id = x[:id] 
+    @name = x[:name] 
+    @type = x[:type] 
+    @db = x[:db]
+    
+    if x[:hp] then @hp = x[:hp] else nil end 
+      
+  end
+  
+  def self.save(name, type, db)
+  
+    db.execute("INSERT INTO pokemon (name, type)
+      VALUES (?, ?)", name, type)
+  
+  end  
+  
+  def self.find(id, db)
+    result = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    store = {:id => result[0][0], :name => result[0][1], :type => result[0][2], :hp => result[0][3], :db => db}
+    Pokemon.new(store)
+  end 
+  
+  def alter_hp(hp,db)
+    self.hp = hp
+    
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
+  
+  end 
+  
 end
